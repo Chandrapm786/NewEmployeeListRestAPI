c29a58af4d47df555a601a2e458add5f14892a10
Chandrapm786 <chandra.pm.786@gmail.com>
2021-04-17 12:11:49 +0530

    Commit to MasterMain branch of Git

---
+++ src/main/java/com/example/demo/File1.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+// This file is added in Initial or First commit to Git
+
+public class File1 {
+    public File1() {
+        System.out.println("This is first file added in Master Branch");
+    }
+}
